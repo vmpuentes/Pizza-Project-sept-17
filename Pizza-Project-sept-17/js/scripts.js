c8cb258943e6fac53df7d6bc91e4d0d22e3cdde4
Victor Puentes <vmpuentes@gmail.com>
2017-09-29 17:02:04 -0700

    add more changes to scripts file.

--- Pizza-Project-sept-17/js/scripts.js
+++ Pizza-Project-sept-17/js/scripts.js
@@ -1,3 +1,36 @@
-$(document).ready(function() {
-  
+//business logic
+function Pizza (toppings,size,crust) {
+  this.toppings = " ";
+  this.size = " ";
+  this.crust - " ";
 }
+
+function Customer (name, order, finalPrice) {
+  this.name = name;
+  this.orger = order;
+  this.finalPrice = price;
+}
+
+Pizza.prototype.getPrice = function () {
+
+}
+
+var orderExtraPizza = new Pizza (toppings, size, crust);
+//user interface logic
+$(document).ready(function() {
+  $("#pizzaUserInput").submit(function(event) {
+    event.preventDefault();
+    var userToppings = $("#toppings").val();
+    var userSize = $("#size").val();
+    var crust = $("#crust").val();
+    var Pizza = new Pizza ();
+    $("#toppins").each(function() {
+      var userToppings = pizza.toppings.append("#toppings").val();
+  });
+    var getPrice = pizza.getPrice();
+    $("#toppings").append(userToppings);
+      $("#size").append(userSize);
+        $("#crust").append(crust);
+          $("#userOrder").append(pizza.getPrice);
+        });
+      });
