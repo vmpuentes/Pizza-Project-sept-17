5b0339a9d72777c1149ca686e31dc44331d2aada
Victor Puentes <vmpuentes@gmail.com>
2017-10-03 19:33:43 -0700

    add new variable orderedPizza, fix prototype getPrice, change output display.

--- Pizza-Project-sept-17/js/scripts.js
+++ Pizza-Project-sept-17/js/scripts.js
@@ -1,37 +1,56 @@
 //business logic
 function Pizza (toppings,size,crust) {
-  this.toppings = " ";
-  this.size = " ";
-  this.crust - " ";
+  this.toppings = toppings;
+  this.size = size;
+  this.crust = crust;
+  this.basePrice = 10;
 }
 
 function Customer (name, order, finalPrice) {
   this.name = name;
-  this.orger = order;
+  this.order = order;
   this.finalPrice = price;
 }
 
 Pizza.prototype.getPrice = function () {
-
+  return (this.basePrice + this.toppings.length * .5);
 }
 
-var orderExtraPizza = new Pizza (toppings, size, crust);
+//var orderExtraPizza = new Pizza (toppings, size, crust);
 //user interface logic
 $(document).ready(function() {
   $("#pizzaUserInput").submit(function(event) {
     event.preventDefault();
-    $("#input:checkbox[name=toppings]:checked").each(function() {
-    var userToppings = $(this).val();
-    var userSize = $("#size").val();
+    var userToppings = [];
+    var pizzaSize = $("#size").val();
     var crust = $("#crust").val();
-    var Pizza = new Pizza ();
-    $("#toppings").each(function() {
-      var userToppings = pizza.toppings.append("#toppings").val();
+
+
+    $("input:checkbox[name=toppings]:checked").each(function() {
+       //$("#toppings").each(function() {
+      //var userToppings = pizza.toppings.append("#toppings").val();
+      var topping = $(this).val();
+      console.log(topping);
+      userToppings.push(topping);
   });
-    var getPrice = pizza.getPrice();
-    $("#orderDetails").append(pizza.getPrice);
-      $("#toppings").append(userToppings);
-        $("#size").append(userSize);
-          $("#crust").append(crust);  
-        });
-      });
+
+  console.log(userToppings);
+
+  var orderedPizza = new Pizza (userToppings, pizzaSize, crust);
+  console.log(orderedPizza.toppings);
+  console.log(orderedPizza.size);
+  console.log(orderedPizza.crust);
+  console.log(orderedPizza);
+  console.log(orderedPizza.getPrice());
+
+    // var getPrice = pizza.getPrice();
+    $("#orderDetails").append(orderedPizza.getPrice());
+
+    // console.log(orderedPizza.crust);
+    // console.log(orderedPizza.size);
+      $("#output").text(orderedPizza.getPrice());
+      //   $("#size").append(userSize);
+      //     $("#crust").append(crust);
+  });
+});
+// });
