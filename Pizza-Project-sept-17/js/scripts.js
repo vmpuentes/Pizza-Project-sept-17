8bcbb49f01ba414f11ef6b1fd14725dbc3fb92c0
vmpuentes <vmpuentes@gmail.com>
2017-10-04 13:29:36 -0700

    Update scripts.js

--- Pizza-Project-sept-17/js/scripts.js
+++ Pizza-Project-sept-17/js/scripts.js
@@ -16,8 +16,7 @@ Pizza.prototype.getPrice = function () {
   return (this.basePrice + this.toppings.length * .5);
 }
 
-//var orderExtraPizza = new Pizza (toppings, size, crust);
-//user interface logic
+
 $(document).ready(function() {
   $("#pizzaUserInput").submit(function(event) {
     event.preventDefault();
@@ -27,30 +26,12 @@ $(document).ready(function() {
 
 
     $("input:checkbox[name=toppings]:checked").each(function() {
-       //$("#toppings").each(function() {
-      //var userToppings = pizza.toppings.append("#toppings").val();
       var topping = $(this).val();
-      console.log(topping);
       userToppings.push(topping);
   });
 
-  console.log(userToppings);
-
   var orderedPizza = new Pizza (userToppings, pizzaSize, crust);
-  console.log(orderedPizza.toppings);
-  console.log(orderedPizza.size);
-  console.log(orderedPizza.crust);
-  console.log(orderedPizza);
-  console.log(orderedPizza.getPrice());
-
-    // var getPrice = pizza.getPrice();
     $("#orderDetails").append(orderedPizza.getPrice());
-
-    // console.log(orderedPizza.crust);
-    // console.log(orderedPizza.size);
       $("#output").text(orderedPizza.getPrice());
-      //   $("#size").append(userSize);
-      //     $("#crust").append(crust);
   });
 });
-// });
